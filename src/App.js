38ed22cc8b12145ec84b9e6ff3ea97f67a4b1fbd
Faigy Stroh <faigyben2@gmail.com>
2023-06-02 17:53:41 +0200

    Gets recipe from user, but passing to the array doesn't work yet

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import Form from "./Components/Form";
 import Header from "./Components/Header";
 import SearchBar from "./Components/SearchBar";
-import {useState} from "react";
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [searchItem, updateSearchItem] = useState();
+  const [recipe, updateRecipe] = useState({});
+  const [recipes, UpdateRecipes] = useState([]);
 
-  const [searchItem,updateSearchItem]= useState()
-  
-
+  function AddNewRecipe(newRecipe) {
+    UpdateRecipes(function (prevArr) {
+      const NewArr = [...prevArr];
+      console.log("NewArr " + NewArr.map(function(recipe){console.log(recipe)}));
+      return [...NewArr, newRecipe];
+    });
+  }
 
   return (
     <>
-    <div className='container'>
-    <Header></Header>
-    <Form></Form>
-    <SearchBar search ={updateSearchItem}></SearchBar>
-    </div>
+      <div className="container">
+        <Header></Header>
+        <Form addrecipe={AddNewRecipe}></Form>
+        <SearchBar search={updateSearchItem}></SearchBar>
+      </div>
     </>
   );
 }
