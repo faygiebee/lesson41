b56eb7380fe9618c0fdd08dbdd50a4545ec6b368
Faigy Stroh <faigyben2@gmail.com>
2023-06-02 17:14:09 +0200

    SearchBar Working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import Form from "./Components/Form";
+import Header from "./Components/Header";
+import SearchBar from "./Components/SearchBar";
+import {useState} from "react";
 import './App.css';
 
 function App() {
+
+  const [searchItem,updateSearchItem]= useState()
+  
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='container'>
+    <Header></Header>
+    <Form></Form>
+    <SearchBar search ={updateSearchItem}></SearchBar>
     </div>
+    </>
   );
 }
 
